a148c4abe65dd92964b86a9dc3ae6530405a8207
J. Randall Hunt <randallhunt@gmail.com>
2013-01-27 14:25:36 -0500

    started working on project, a few initial classes

--- mechanics.py
+++ mechanics.py
@@ -3,23 +3,81 @@
 import colorama
 import sys
 from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
 
 
-def parse_command(commandstr):
-    print commandstr
+class BaseAdventure:
+    """This is a base class for all of our adventure objects."""
+
+    def __init__(self, name, output=sys.stdout):
+        self.name = name
+        self.output = output
+        self.description = ""
+
+    def out(self, text):
+        """Write the text to the object's stream variable.
+
+        Output text to the output, in this way an adventure
+        could be instantiated such that the output was to a file, perhaps
+        for HTML, rather than soley to the screen.
+        """
+        self.output.write(text)
+
+    def look(self):
+        self.out(self.description)
 
 
-def run():
-    prompt_str = '\n' + Fore.RED + '> '
-    try:
-        while True:
-            commandstr = raw_input(prompt_str).lower().strip()
-            if not commandstr:
-                continue
-            parse_command(commandstr)
-    except KeyboardInterrupt:
-        sys.exit()
+class Character(BaseAdventure):
+    def __init__(self, name, startarea):
+        self.name = name
+        self.location = startarea
+        self.inventory = dict()
+
+    def move(self, direction):
+        if self.location.exits[direction]:
+            self.location = self.location.exits[direction]
+            self.look()
+
+    def look(self):
+        print self.location.look()
+
+
+class Area(BaseAdventure):
+    # it my be cool to try and use with Area(blarg) as room:
+    # we would need to define __enter__ and __exit__
+    def __init__(self, name):
+        self.exits = dict()
+
+    def addPath(self, direction, area):
+        self.exits[direction] = area
+
+
+class Game:
+    def __init__(self):
+        first = Area("first")
+        first.description = "this is first area"
+        second = Area("second")
+        second.description = "this is the second area"
+        first.addPath("south", second)
+        self.maincharacter = Character("ranman", first)
+
+    def parse_command(self, commandstr):
+        command = commandstr.split()
+        if "go" == command[0]:
+            self.maincharacter.move(command[1])
+
+    def run(self):
+        colorama.init(autoreset=True)  # automatically reset colors
+        prompt_str = ''.join(['\n', Fore.RED, '> '])
+        try:
+            while True:
+                commandstr = raw_input(prompt_str).lower().strip()
+                if not commandstr:
+                    continue
+                self.parse_command(commandstr)
+        except (KeyboardInterrupt, EOFError):
+            print "\n Good Game!"
+            sys.exit()
 
 if __name__ == '__main__':
-    run()
+    game = Game()
+    game.run()
